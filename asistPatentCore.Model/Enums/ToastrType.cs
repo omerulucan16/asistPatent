922058dcafa01943c0e0ec715eb52fb0a7e597c1
omerulucan16 <omerulucan@windowslive.com>
2020-07-17 11:46:09 +0300

    UserRoles

---
+++ asistPatentCore.Model/Enums/ToastrType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asistPatentCore.Model.Enums
+{
+    public enum ToastrType
+    {
+        Info = 0,
+        Success = 1,
+        Warning = 2,
+        Error = 3
+    }
+}
---
+++ asistPatentCore.Model/Enums/UserRoleEnum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace asistPatentCore.Model.Enums
+{
+    public enum UserRoleEnum
+    {
+        customer = 0,
+        admin = 1,
+        supervisor = 2
+    }
+}
